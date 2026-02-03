1b7e60cc1ea3e94e99aa03512798117170e3b261
Łukasz Rutkowski <lukus178@gmail.com>
2026-02-03 18:41:03 +0100

    Task modifier with debouncer parameter

--- Sources/DebouncedOnChange/DebouncedTaskViewModifier.swift
+++ Sources/DebouncedOnChange/DebouncedTaskViewModifier.swift
@@ -32,4 +32,66 @@ extension View {
             await action()
         }
     }
+
+    /// Adds a task to perform when this view appears or when a specified value changes and a specified
+    /// `debounceTime` elapses between value changes.
+    ///
+    /// Each time the value changes before `debounceTime` passes, the previous action will be cancelled and the next
+    /// action will be scheduled to run after that time passes again. This mean that the action will only execute
+    /// after changes to the value stay unmodified for the specified `debounceTime`.
+    ///
+    /// - Parameters:
+    ///   - value: The value to observe for changes. The value must conform to the `Equatable` protocol.
+    ///   - duration: The time to wait after each value change before running `action` closure.
+    ///   - debouncer: Object used to manually cancel debounced actions.   
+    ///   - action: A closure called after debounce time as an asynchronous task before the view appears. SwiftUI can
+    ///     automatically cancel the task after the view disappears before the action completes. If the id value
+    ///     changes, SwiftUI cancels and restarts the task.
+    /// - Returns: A view that runs the specified action asynchronously before the view appears, or restarts the task
+    ///     with the id value changes after debounced time.
+    @available(iOS 17, *)
+    @available(macOS 14.0, *)
+    @available(tvOS 17.0, *)
+    @available(watchOS 10.0, *)
+    @available(visionOS 1.0, *)
+    public func task<T>(
+        id value: T,
+        priority: TaskPriority = .userInitiated,
+        debounceTime: Duration,
+        debouncer: Binding<Debouncer>,
+        _ action: @escaping () async -> Void
+    ) -> some View where T: Equatable {
+        self.modifier(
+            DebouncedTaskViewModifier(
+                trigger: value,
+                action: action,
+                debounceDuration: debounceTime,
+                debouncer: debouncer
+            )
+        )
+    }
+}
+
+@available(iOS 17, *)
+@available(macOS 14.0, *)
+@available(tvOS 17.0, *)
+@available(watchOS 10.0, *)
+@available(visionOS 1.0, *)
+private struct DebouncedTaskViewModifier<Value>: ViewModifier where Value: Equatable {
+    let trigger: Value
+    let action: () async -> Void
+    let debounceDuration: Duration
+    let debouncer: Binding<Debouncer>
+
+    @State private var debouncedTask: Task<Void, Never>?
+
+    func body(content: Content) -> some View {
+        content.onChange(of: trigger, initial: true) {
+            debouncedTask?.cancel()
+            debouncedTask = Task.delayed(duration: debounceDuration) {
+                await action()
+            }
+            debouncer.wrappedValue.task = debouncedTask
+        }
+    }
 }
